3a09cb6396574ff918b7c9cf6d0780d686c9c6c2
DascaluCosmin <dascalu.ac@gmail.com>
2022-02-24 20:20:28 +0200

    [S13: Notifications] - PeriodicEvent Model class

--- app/src/main/java/com/ubb/citizen_u/data/model/events/Event.kt
+++ app/src/main/java/com/ubb/citizen_u/data/model/events/Event.kt
@@ -58,7 +58,7 @@ data class PublicEvent(
     }
 }
 
-data class PublicReleaseEvent(
+open class PublicReleaseEvent(
     var publicationDate: Date? = null,
     var headline: Map<String, String> = mapOf(),
 ) : Event() {
@@ -86,4 +86,53 @@ data class PublicReleaseEvent(
                 "Publication Date = $publicationDate, " +
                 "Headline = ${headline[DEFAULT_LANGUAGE]}"
     }
+}
+
+data class PeriodicEvent(
+    var frequency: PeriodicEventFrequency? = null,
+    var happeningDay: PeriodicEventHappeningDay? = null,
+    var happeningMonth: PeriodicEventHappeningMonth? = null,
+) : PublicReleaseEvent() {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as PeriodicEvent
+
+        if (frequency != other.frequency) return false
+        if (happeningDay != other.happeningDay) return false
+        if (happeningMonth != other.happeningMonth) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + (frequency?.hashCode() ?: 0)
+        result = 31 * result + (happeningDay?.hashCode() ?: 0)
+        result = 31 * result + (happeningMonth?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "${super.toString()}, " +
+                "Frequency = $frequency, " +
+                "Happening Day = $happeningDay" +
+                "Happening Month = $happeningMonth"
+    }
+}
+
+
+enum class PeriodicEventFrequency {
+    WEEKLY, MONTHLY, ANNUALLY
+}
+
+enum class PeriodicEventHappeningDay {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+enum class PeriodicEventHappeningMonth {
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
 }
